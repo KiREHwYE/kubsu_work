7195d6ef1e6e507156972ba13a9a3ce930dce600
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-04-22 21:55:31 +0300

    Update index.php

--- ex3/index.php
+++ ex3/index.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
     print('Спасибо, результаты сохранены.');
-    exit()
+    exit();
   }
   include('index.html');
   exit();
