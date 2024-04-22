1553d097cd2460e444ac6fdf58913d181b22aad8
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-04-22 23:22:00 +0300

    Update form.php

--- ex4/form.php
+++ ex4/form.php
@@ -1,57 +1,3 @@
-<!--<!doctype html>
-<html class="no-js" lang="">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exercise 2</title>
-</head>
-
-<body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-
-<h1>
-  Form
-</h1>
-
-<form style="display: flex;flex-direction: column;width: 20%">
-
-  <input required type="text" name="name" placeholder="Full name">
-  <input required type="tel" name="phone" placeholder="Phone number">
-  <input required type="email" name="email" placeholder="Email">
-  <input required type="date" name="year" placeholder="Date of birth">
-
-  <div style="flex-direction: row;margin-top: 20px">
-    <input required type="radio" name="sex" value="Male">Male
-    <input required type="radio" name="sex" value="Female">Female
-  </div>
-
-  <select style="margin-top: 20px" name="language">
-    <option value="value1">Pascal</option>
-    <option value="value2">C</option>
-    <option value="value3">C++</option>
-    <option value="value4">JavaScript</option>
-    <option value="value5">PHP</option>
-    <option value="value6">Python</option>
-    <option value="value7">Java</option>
-    <option value="value8">Haskel</option>
-    <option value="value9">Clojure</option>
-    <option value="value10">Prolog</option>
-    <option value="value11">Scala</option>
-  </select>
-
-  <textarea required style="margin-top: 20px" name="biography" placeholder="Your biography"></textarea>
-
-  <p><input required type="checkbox" name="contract_agreement" value="yes">I agree with the contract.</p>
-
-  <input required type="submit" value="Submit">
-
-</form>
-
-</body>
-
-</html>-->
-
-
 <html>
 <head>
   <style>
@@ -84,8 +30,8 @@ print('</div>');
   <input required type="date" name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" placeholder="Date of birth">
 
   <div style="flex-direction: row;margin-top: 20px">
-    <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="<?php print $values['sex']; ?>" value="Male">Male
-    <input required type="radio" name="sex" value="Female">Female
+    <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="<?php print $values['sex']; ?>" value="M">Male
+    <input required type="radio" name="sex" value="F">Female
   </div>
 
   <select style="margin-top: 20px" name="language[]" multiple <?php if ($errors['language']) {print 'class="error"';} ?> value="<?php print $values['language']; ?>" >
