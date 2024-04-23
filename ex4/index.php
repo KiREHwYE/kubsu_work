7b8f330bf1256b9e1013a29e61418c5c1a2ed96b
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-04-23 18:18:17 +0300

    Update index.php

--- ex4/index.php
+++ ex4/index.php
@@ -155,13 +155,31 @@ else {
                             setcookie(strval($selectedOption) + '_value', $_POST[strval($selectedOption)], time() + 30 * 24 * 60 * 60);
                     }
 
-                    if (empty($_POST['biography']) || strlen($_POST['biography']) > 256) {
-                                // Выдаем куку на день с флажком об ошибке в поле fio.
-                                setcookie('biography_error', '1', time() + 24 * 60 * 60);
-                                $errors = TRUE;
+//                     if (empty($_POST['biography']) || strlen($_POST['biography']) > 256) {
+//                                 // Выдаем куку на день с флажком об ошибке в поле fio.
+//                                 setcookie('biography_error', '1', time() + 24 * 60 * 60);
+//                                 $errors = TRUE;
+//                               }
+//                               // Сохраняем ранее введенное в форму значение на месяц.
+//                               setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
+                              if (empty($_POST['biography']) || strlen($_POST['biography']) > 256) {
+                                  // Set a cookie for one day with a flag indicating an error in the biography field.
+                                  setcookie('biography_error', '1', time() + 24 * 60 * 60);
+                                  $errors = TRUE;
+                              } else {
+                                  // Save the previously entered value in the form for one month.
+                                  setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
                               }
-                              // Сохраняем ранее введенное в форму значение на месяц.
-                              setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
+
+                              // Use the 'biography_value' cookie to restore the biography text
+                              $biography_value = isset($_COOKIE['biography_value']) ? $_COOKIE['biography_value'] : '';
+
+                              // Corrected <textarea> element
+                              echo '<textarea required style="margin-top: 20px" name="biography"';
+                              if ($errors['biography']) {
+                                  echo ' class="error"';
+                              }
+                              echo ' placeholder="Your biography">' . htmlspecialchars($biography_value) . '</textarea>';
 
 // *************
 // TODO: тут необходимо проверить правильность заполнения всех остальных полей.
