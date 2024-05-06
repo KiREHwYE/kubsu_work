9db31eb3c06c18a541b4243c4d931c041db4057b
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-06 19:10:45 +0300

    Update index.php

--- ex6/index.php
+++ ex6/index.php
@@ -109,9 +109,9 @@ $values['language'] = explode(',', $savedLanguage);
   $values['biography'] = empty($_COOKIE['biography_value']) ? '' : strip_tags($_COOKIE['biography_value']);
   $values['contract_agreement'] = empty($_COOKIE['contract_agreement_value']) ? '' : strip_tags($_COOKIE['contract_agreement_value']);
 
-function isSelected($optionValue, $savedLanguages) {
-    return in_array($optionValue, $savedLanguages) ? 'selected' : '';
-}
+function isSelected($optionValue, $values['language']) {
+        return in_array($optionValue, $values['language']) ? 'selected' : '';
+    }
   
   // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
   // ранее в сессию записан факт успешного логина.
