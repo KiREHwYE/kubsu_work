df7ec5d44d37819e7812be058a0dbc25a3538289
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-06 17:22:45 +0300

    Add files via upload

---
+++ ex6/form.php
@@ -0,0 +1,113 @@
+<!--<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercise 2</title>
+</head>
+
+<body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+
+<h1>
+  Form
+</h1>
+
+<form style="display: flex;flex-direction: column;width: 20%">
+
+  <input required type="text" name="name" placeholder="Full name">
+  <input required type="tel" name="phone" placeholder="Phone number">
+  <input required type="email" name="email" placeholder="Email">
+  <input required type="date" name="year" placeholder="Date of birth">
+
+  <div style="flex-direction: row;margin-top: 20px">
+    <input required type="radio" name="sex" value="Male">Male
+    <input required type="radio" name="sex" value="Female">Female
+  </div>
+
+  <select style="margin-top: 20px" name="language">
+    <option value="value1">Pascal</option>
+    <option value="value2">C</option>
+    <option value="value3">C++</option>
+    <option value="value4">JavaScript</option>
+    <option value="value5">PHP</option>
+    <option value="value6">Python</option>
+    <option value="value7">Java</option>
+    <option value="value8">Haskel</option>
+    <option value="value9">Clojure</option>
+    <option value="value10">Prolog</option>
+    <option value="value11">Scala</option>
+  </select>
+
+  <textarea required style="margin-top: 20px" name="biography" placeholder="Your biography"></textarea>
+
+  <p><input required type="checkbox" name="contract_agreement" value="yes">I agree with the contract.</p>
+
+  <input required type="submit" value="Submit">
+
+</form>
+
+</body>
+
+</html>-->
+
+
+<html>
+<head>
+  <style>
+    /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+    .error {
+      border: 2px solid red;
+    }
+  </style>
+</head>
+
+<body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+// Выводим все сообщения.
+foreach ($messages as $message) {
+print($message);
+}
+print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+
+<form style="display: flex;flex-direction: column;width: 20%" action="" method="POST">
+  <input required type="text" name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" placeholder="Full name">
+  <input required type="tel" name="phone" <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>" placeholder="Phone number">
+  <input required type="email" name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="Email">
+  <input required type="date" name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" placeholder="Date of birth">
+
+  <div style="flex-direction: row;margin-top: 20px">
+      <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="M" <?php if ($values['sex'] == 'M') {print 'checked';} ?>>Male
+      <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="F" <?php if ($values['sex'] == 'F') {print 'checked';} ?>>Female
+  </div>
+
+  <select style="margin-top: 20px" name="language[]" multiple <?php if ($errors['language']) {print 'class="error"';} ?>>
+    <option value="value1">Pascal</option>
+    <option value="value2">C</option>
+    <option value="value3">C++</option>
+    <option value="value4">JavaScript</option>
+    <option value="value5">PHP</option>
+    <option value="value6">Python</option>
+    <option value="value7">Java</option>
+    <option value="value8">Haskel</option>
+    <option value="value9">Clojure</option>
+    <option value="value10">Prolog</option>
+    <option value="value11">Scala</option>
+  </select>
+
+  <textarea required style="margin-top: 20px" name="biography" <?php if ($errors['biography']) {print 'class="error"';} ?> placeholder="Your biography"><?php print htmlspecialchars($values['biography']); ?></textarea>
+
+  <p><input required type="checkbox" name="contract_agreement" <?php if ($errors['contract_agreement']) {print 'class="error"';} ?> value="Yes" <?php if ($values['contract_agreement'] == 'Yes') {print 'checked';} ?>>I agree with the contract.</p>
+
+  <input required type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ ex6/login.php
@@ -0,0 +1,102 @@
+<?php
+
+define("user", "u67397");
+define("password", "2392099");
+define("dbname", "u67397");
+
+/**
+ * Файл login.php для не авторизованного пользователя выводит форму логина.
+ * При отправке формы проверяет логин/пароль и создает сессию,
+ * записывает в нее логин и id пользователя.
+ * После авторизации пользователь перенаправляется на главную страницу
+ * для изменения ранее введенных данных.
+ **/
+
+// Отправляем браузеру правильную кодировку,
+// файл login.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SESSION хранятся переменные сессии.
+// Будем сохранять туда логин после успешной авторизации.
+$session_started = false;
+if ($_COOKIE[session_name()] && session_start()) {
+  $session_started = true;
+  if (!empty($_SESSION['login'])) {
+    // Если есть логин в сессии, то пользователь уже авторизован.
+    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
+    session_destroy();
+    //при нажатии на кнопку Выход).
+    // Делаем перенаправление на форму.
+    header('Location: ./');
+    exit();
+  }
+}
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+?>
+
+<form style="display: flex; flex-direction: column; justify-content: center; align-items: center" action="" method="post">
+  <input name="login" />
+  <input name="pass" />
+  <input type="submit" value="Войти" />
+</form>
+
+<?php
+}
+// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+else {
+      if (!$session_started) {
+        session_start();
+      }
+
+      $user = user;
+      $pass = password;
+      $db = new PDO('mysql:host=localhost;dbname=' . dbname, $user, $pass, [
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+      ]);
+
+      try {
+        // Проверяем, есть ли пользователь с таким логином и паролем
+        $login = $_POST['login'];
+        $password = $_POST['pass'];
+
+        $stmt = $db->prepare("SELECT personId FROM personAuthentificationData WHERE login = :login AND pass = :pass");
+        $stmt->execute([':login' => $login, ':pass' => $password]);
+        $authData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($authData) {
+          // Пользователь существует, сохраняем данные в сессию
+          $_SESSION['login'] = $login;
+          $_SESSION['uid'] = $authData['personId'];
+
+          $stmt = $db->prepare("SELECT name, phone, email, year, sex, biography FROM person WHERE personId = :$authData['personId']");
+          $stmt->execute([
+            ':name' => $name,
+            ':phone' => $phone,
+            ':email' => $email,
+            ':year' => $year,
+            ':sex' => $sex,
+            ':biography' => $biography,
+            ]);
+
+          // Делаем перенаправление на главную страницу
+          header('Location: ./');
+          exit();
+        } else {
+          // Пользователь не найден, выдаем ошибку
+          echo "Неверный логин или пароль.";
+          exit();
+        }
+      } catch(PDOException $e){
+        print('Error : ' . $e->getMessage());
+        exit();
+      }
+
+
+
+  // Делаем перенаправление.
+  header('Location: ./');
+}
