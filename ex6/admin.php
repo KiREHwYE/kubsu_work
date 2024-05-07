c6b8e2a7aaf1e73042ff4468676fd827b7bf3c4f
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-07 22:02:55 +0300

    Update admin.php

--- ex6/admin.php
+++ ex6/admin.php
@@ -264,8 +264,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['userId'])) {
             $stmt = $db->prepare("DELETE FROM person WHERE personId = :personId");
             $stmt->execute([':personId' => $_POST['personId']]);
 
-            header("Location: ".$_SERVER['REQUEST_URI']);
-
         } catch (PDOException $e) {
             echo "Ошибка выполнения запроса: " . $e->getMessage();
         }
