ff3e64d1e8186211a3bd0f1ad01e371fc8ed48f7
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-07 18:11:16 +0300

    Update admin.php

--- ex6/admin.php
+++ ex6/admin.php
@@ -8,10 +8,48 @@
 // Пример HTTP-аутентификации.
 // PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
 // Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
+
+define("user", "u67397");
+define("password", "2392099");
+define("dbname", "u67397");
+
+$isAdminAuth = false;
+
+$user = user;
+$pass = password;
+$db = new PDO('mysql:host=localhost;dbname=' . dbname, $user, $pass, [
+    PDO::ATTR_PERSISTENT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+if (!empty($_SERVER['PHP_AUTH_USER']) &&
+    !empty($_SERVER['PHP_AUTH_PW'])) {
+
+    try {
+        // Проверяем, есть ли пользователь с таким логином и паролем
+        $admin_login = $_SERVER['PHP_AUTH_USER'];
+        $admin_pass = $_SERVER['PHP_AUTH_PW'];
+        $md5Pass = md5($admin_pass);
+
+        $stmt = $db->prepare("SELECT * FROM adminAccount WHERE adminLogin = :adminLogin AND adminPass = :adminPass");
+        $stmt->execute([':adminLogin' => $login, ':adminPass' => $md5Pass]);
+        $authData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($authData) {
+            $isAdminAuth = true;
+        }
+    } catch(PDOException $e){
+        print('Error : ' . $e->getMessage());
+        exit();
+    }
+}
+
+
+
 if (empty($_SERVER['PHP_AUTH_USER']) ||
     empty($_SERVER['PHP_AUTH_PW']) ||
-    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
+    !$isAdminAuth) {
+
   header('HTTP/1.1 401 Unanthorized');
   header('WWW-Authenticate: Basic realm="My site"');
   print('<h1>401 Требуется авторизация</h1>');
@@ -24,3 +62,11 @@ print('Вы успешно авторизовались и видите защи
 // Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
 // Реализовать просмотр и удаление всех данных.
 // *********
+
+<body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+
+<h1>
+  Admin Control Center
+</h1>
+
+</body>
