6b95a27dd878bf6d4c1570e85dd4459ac4a17526
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-20 16:41:37 +0300

    Update index.php

--- ex7/index.php
+++ ex7/index.php
@@ -6,9 +6,7 @@ $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 $dbName = getenv('DB_NAME');
 
-$user = $dbUser;
-$pass = $dbPassword;
-$db = new PDO('mysql:host=localhost;dbname=' . $dbName, $user, $pass, [
+$db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser, $dbPassword, [
     PDO::ATTR_PERSISTENT => true,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
