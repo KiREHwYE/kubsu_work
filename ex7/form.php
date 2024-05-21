03af68fff45354a8707447cd7a074ce7f3c44241
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-21 20:32:29 +0300

    Update form.php

--- ex7/form.php
+++ ex7/form.php
@@ -10,85 +10,59 @@ if (!isset($_SESSION['csrf_token'])) {
 
 $csrfToken = $_SESSION['csrf_token'];
 
-if (!empty($messages)) {
-    print('<div id="messages">');
-
-    // Выводим все сообщения.
-    foreach ($messages as $message) {
-        print($message);
-    }
-
-    print('</div>');
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header('Location: ./login.php');
+    exit();
 }
 
-if (isset($_COOKIE[session_name()]) && session_status() != PHP_SESSION_NONE) {
-    if (!empty($_SESSION['login'])) {
-        if (isset($_POST['logout'])) {
-            session_destroy();
-            header('Location: ./login.php');
-            exit();
-        }
-
-        // Делаем перенаправление на форму.
-        header('Location: ./');
-        exit();
-    }
+if (!empty($_SESSION['login'])) {
+    header('Location: ./');
+    exit();
 }
+
 ?>
 
 <html>
 <head>
     <style>
-        /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
         .error {
             border: 2px solid red;
         }
     </style>
 </head>
-
 <body style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-
-<h1>
-    Form
-</h1>
-
-<form style="display: flex;flex-direction: column;width: 20%" action="" method="POST">
-    <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
-    <input required type="text" name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" placeholder="Full name">
-    <input required type="tel" name="phone" <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>" placeholder="Phone number">
-    <input required type="email" name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="Email">
-    <input required type="date" name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" placeholder="Date of birth">
-
-    <div style="flex-direction: row;margin-top: 20px">
-        <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="M" <?php if ($values['sex'] == 'M') {print 'checked';} ?>>Male
-        <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="F" <?php if ($values['sex'] == 'F') {print 'checked';} ?>>Female
-    </div>
-
-    <select style="margin-top: 20px" name="language[]" multiple <?php if ($errors['language']) {print 'class="error"';} ?>>
-        <option value="Pascal" <?php echo isSelected('Pascal', $savedLanguages); ?>>Pascal</option>
-        <option value="C" <?php echo isSelected('C', $savedLanguages); ?>>C</option>
-        <option value="C++" <?php echo isSelected('C++', $savedLanguages); ?>>C++</option>
-        <option value="JavaScript" <?php echo isSelected('JavaScript', $savedLanguages); ?>>JavaScript</option>
-        <option value="PHP" <?php echo isSelected('PHP', $savedLanguages); ?>>PHP</option>
-        <option value="Python" <?php echo isSelected('Python', $savedLanguages); ?>>Python</option>
-        <option value="Java" <?php echo isSelected('Java', $savedLanguages); ?>>Java</option>
-        <option value="Haskel" <?php echo isSelected('Haskel', $savedLanguages); ?>>Haskel</option>
-        <option value="Clojure" <?php echo isSelected('Clojure', $savedLanguages); ?>>Clojure</option>
-        <option value="Prolog" <?php echo isSelected('Prolog', $savedLanguages); ?>>Prolog</option>
-        <option value="Scala" <?php echo isSelected('Scala', $savedLanguages); ?>>Scala</option>
-    </select>
-
-    <textarea required style="margin-top: 20px" name="biography" <?php if ($errors['biography']) {print 'class="error"';} ?> placeholder="Your biography"><?php print htmlspecialchars($values['biography']); ?></textarea>
-
-    <p><input required type="checkbox" name="contract_agreement" <?php if ($errors['contract_agreement']) {print 'class="error"';} ?> value="Yes" <?php if ($values['contract_agreement'] == 'Yes') {print 'checked';} ?>>I agree with the contract.</p>
-
-    <input required type="submit" value="Submit">
-</form>
-
-<form method="post">
-    <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
-    <input type="submit" name="logout" value="Выйти" />
-</form>
-
+    <h1>Form</h1>
+    <form style="display: flex; flex-direction: column; width: 20%" action="" method="POST">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
+        <input required type="text" name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" placeholder="Full name">
+        <input required type="tel" name="phone" <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>" placeholder="Phone number">
+        <input required type="email" name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="Email">
+        <input required type="date" name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>" placeholder="Date of birth">
+        <div style="flex-direction: row; margin-top: 20px">
+            <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="M" <?php if ($values['sex'] == 'M') {print 'checked';} ?>>Male
+            <input required type="radio" name="sex" <?php if ($errors['sex']) {print 'class="error"';} ?> value="F" <?php if ($values['sex'] == 'F') {print 'checked';} ?>>Female
+        </div>
+        <select style="margin-top: 20px" name="language[]" multiple <?php if ($errors['language']) {print 'class="error"';} ?>>
+            <option value="Pascal" <?php echo isSelected('Pascal', $savedLanguages); ?>>Pascal</option>
+            <option value="C" <?php echo isSelected('C', $savedLanguages); ?>>C</option>
+            <option value="C++" <?php echo isSelected('C++', $savedLanguages); ?>>C++</option>
+            <option value="JavaScript" <?php echo isSelected('JavaScript', $savedLanguages); ?>>JavaScript</option>
+            <option value="PHP" <?php echo isSelected('PHP', $savedLanguages); ?>>PHP</option>
+            <option value="Python" <?php echo isSelected('Python', $savedLanguages); ?>>Python</option>
+            <option value="Java" <?php echo isSelected('Java', $savedLanguages); ?>>Java</option>
+            <option value="Haskel" <?php echo isSelected('Haskel', $savedLanguages); ?>>Haskel</option>
+            <option value="Clojure" <?php echo isSelected('Clojure', $savedLanguages); ?>>Clojure</option>
+            <option value="Prolog" <?php echo isSelected('Prolog', $savedLanguages); ?>>Prolog</option>
+            <option value="Scala" <?php echo isSelected('Scala', $savedLanguages); ?>>Scala</option>
+        </select>
+        <textarea required style="margin-top: 20px" name="biography" <?php if ($errors['biography']) {print 'class="error"';} ?> placeholder="Your biography"><?php print htmlspecialchars($values['biography']); ?></textarea>
+        <p><input required type="checkbox" name="contract_agreement" <?php if ($errors['contract_agreement']) {print 'class="error"';} ?> value="Yes" <?php if ($values['contract_agreement'] == 'Yes') {print 'checked';} ?>>I agree with the contract.</p>
+        <input required type="submit" value="Submit">
+    </form>
+    <form method="post">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
+        <input type="submit" name="logout" value="Выйти" />
+    </form>
 </body>
 </html>
