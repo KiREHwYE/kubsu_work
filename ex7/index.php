6035915c9bb430fe9f24c508bd7a36176765666d
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-21 20:18:28 +0300

    Update index.php

--- ex7/index.php
+++ ex7/index.php
@@ -97,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         return in_array($optionValue, $savedLanguages) ? 'selected' : '';
   }
 
-  if (session_start() && !empty($_SESSION['login'])) {
+  if (session_status() != PHP_SESSION_NONE && !empty($_SESSION['login'])) {
 
     try {
       $stmt = $db->prepare("SELECT name, phone, email, year, sex, biography FROM person WHERE personId = :personId");
