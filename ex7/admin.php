bedcaea8cec0a01ea9d3aa440411664e74de96ad
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2024-05-21 19:39:33 +0300

    Update admin.php

--- ex7/admin.php
+++ ex7/admin.php
@@ -20,6 +20,10 @@ foreach ($lines as $line) {
 
 $isAdminAuth = false;
 
+$dbUser = $_ENV['DB_USER'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_NAME'];
+
 $db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser, $dbPassword, [
     PDO::ATTR_PERSISTENT => true,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
